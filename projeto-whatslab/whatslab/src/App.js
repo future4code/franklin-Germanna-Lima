2e00d2b9aad4dcd5c69e10db26beeda6b9b73c03
germannafol_linux <germannafol@outlook.com>
2022-06-03 17:04:48 -0300

    Atualizaçao CSS

--- projeto-whatslab/whatslab/src/App.js
+++ projeto-whatslab/whatslab/src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
-// import './coletaTexto.js';
+import styled from 'styled-components';
+
+const CaixaTotal= styled.div`
+  flex: 1 1;
+  flex-direction: column;
+  height: 39.2rem;
+  border: 2px solid rgb(0, 0, 0);
+  background: #80808026;
+`;
+
+const CaixaEscrita=styled.div`
+display: flex;
+`;
 
 function App() {
   return (
-    <div className="caixa-total">
+    <CaixaTotal>
       <div className="caixa-enviado"></div>
-      <div className="caixa-escrita">
+      <CaixaEscrita className="caixa-escrita">
         <input type="text" className="usuario" placeholder="Usuário" value=""></input>
         <input type="text" className="mensagem" placeholder="Mensagem" value=""></input>
-        <button type="submit" onClick="texto{ console.log('texto')}" className='botao'>Enviar</button>
-      </div>
-    </div>
+        <button type="submit" onClick="texto {console.log('texto')}" className='botao'>Enviar</button>
+      </CaixaEscrita>
+    </CaixaTotal>
   );
 }
 
