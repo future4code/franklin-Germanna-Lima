3f43982084e7a4c1dad85a966f68407db0cd947b
germannafol_linux <germannafol@outlook.com>
2022-07-24 10:53:30 -0300

    Atualização até o item 3.b

--- insta4/insta4/src/App.js
+++ insta4/insta4/src/App.js
@@ -9,14 +9,28 @@ const MainContainer = styled.div`
   align-items: center;
 `
 function App() {
-return(
-  <MainContainer>
+  const listaNomes = ['paulinha', 'maria', 'joice'];
+  const listaFotoUsuarios = ['https://picsum.photos/50/50', 'https://picsum.photos/50/50', 'https://picsum.photos/50/50'];
+  const listaFotoPosts = ['https://picsum.photos/200/150', 'https://picsum.photos/200/150', 'https://picsum.photos/200/150']
+  
+  return(
+    <MainContainer>
            <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
+            nomeUsuario={listaNomes[0]}
+            fotoUsuario={listaFotoUsuarios[0]}
+            fotoPost={listaFotoPosts [0]}
           />
-  </MainContainer>
+            <Post
+            nomeUsuario={listaNomes[1]}
+            fotoUsuario={listaFotoUsuarios[1]}
+            fotoPost={listaFotoPosts [1]}
+          />
+            <Post
+            nomeUsuario={listaNomes[2]}
+            fotoUsuario={listaFotoUsuarios[2]}
+            fotoPost={listaFotoPosts [2]}
+          />
+    </MainContainer>
 )
 
 }
--- insta4/insta4/src/components/Post/Post.js
+++ insta4/insta4/src/components/Post/Post.js
@@ -39,24 +39,29 @@ const UserPhoto = styled.img`
 const PostPhoto = styled.img`
   width: 100%;
 `
-
+//Componente Post que inializa 4 propriedades
 function Post(props){
   const [state, setState] = useState({
+    // Usado para a condicional do icone mudar ao ser curtido
     curtido: false,
+    //Contador de curtidas
     numeroCurtidas: 0,
+    // Usado para a condicional se está comentado ou não. Ao clicar muda o estado de false para true
     comentando: false,
+    //Contador de comentários
     numeroComentarios: 0
   })
-
+//desestruturação?
   const [numeroCurtidas, setnumeroCurtidas] = useState (0)
   const [curtido, setCurtido] = useState(false)
   const [comentando, setComentando] = useState(false)
   const [numeroComentarios, setNumeroComentarios] = useState(0)
-
+//Função que gera um console.log toda vez que é clicado no icone
   const onClickCurtida = () => {
     console.log('Curtiu!')
   }
   
+  //Variável comentado começa false, ao ser clicada, muda para true e gera o JSX com o espaço para ser comentado + gera um console.log que mostra se true ouy false
   const onClickComentario = () => {
     setComentando(!comentando)
     if(comentando) {
@@ -64,14 +69,18 @@ function Post(props){
     }
     console.log(comentando)
   }
-  
+  //Ao clicar em enviar o comentario, muda a variável comentando para false e add ao contador mais um comentário
   const aoEnviarComentario = () => {
     setComentando(false)
     setNumeroComentarios(numeroComentarios + 1)
   }
 
-  let iconeCurtida
+  //Renderizar coração
+  const handlePhoto = (event) => {
+    onClickCurtida (event.target.value)
+  }
 
+    let iconeCurtida
     if(curtido) {
       iconeCurtida = iconeCoracaoPreto
     } else {
@@ -79,35 +88,43 @@ function Post(props){
     }
 
     let componenteComentario
-
     if(comentando) {
       componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
     }
 
   return(
     <PostContainer>
+      
       <PostHeader>
         <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
         <p>{props.nomeUsuario}</p>
       </PostHeader>
 
       <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
-
+      {/* JSX */}
       <PostFooter>
+        {/* Componente */}
         <IconeComContador
+          // condicional que muda a cor do coração do icone da curtida
           icone={iconeCurtida}
-          onClickIcone={onClickCurtida}
+          //Propriedade onClickIcone do reack que modifica ao ser clicado + Uma arrow fuction que gera um console.log que informa quando é curtido
+          onClickIcone={handlePhoto}
+          //Hook useState que muda o numero de curtidas na variável numeroCurtidas + ! Não está usando o handleInput para atualizar a tela
           valorContador={state.numeroCurtidas}
         />
 
         <IconeComContador
+          //import do icone de comentário
           icone={iconeComentario}
+          //Propriedade onClickIcone do reack que modifica ao ser clicado +  ao ser clicado seta comentario como false e fecha a seção de escrita do comentário + condicional com comentado true gera um componenteComentario que recebe um jsx secaoComentario + props aoEnviar
           onClickIcone={onClickComentario}
+          //Hook useState que muda o numero de curtidas na variável numeroComentarios + ! Não está usando o handleInput para atualizar a tela
           valorContador={numeroComentarios}
         />
       </PostFooter>
       {componenteComentario}
     </PostContainer>
+    
   )
 }
 
